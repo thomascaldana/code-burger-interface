83fa7a478599e3f426f5924719511140a75aeb31
thomascaldana <thomascaldana@gmail.com>
2023-06-20 13:40:00 -0300

    eslint installed

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
-function Login() {
+function Login () {
   return (
     <div>
       <h1>Login Container</h1>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Login
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Login from "./containers/Login";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Login from './containers/Login'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Login />
   </React.StrictMode>
-);
+)
