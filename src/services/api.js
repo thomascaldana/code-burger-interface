2a8ee00f37792276ed694f406e8c97c147e820e5
thomascaldana <thomascaldana@gmail.com>
2023-07-09 14:54:08 -0300

    updating baseURL in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://backend-code-burger-production.up.railway.app/'
 })
 
 apiCodeBurger.interceptors.request.use(async config => {
