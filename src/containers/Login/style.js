7b36fbaa87fb6184de867201c75ca9ce2ea88632
thomascaldana <thomascaldana@gmail.com>
2023-07-11 21:15:19 -0300

    making the login page responsive

--- src/containers/Login/style.js
+++ src/containers/Login/style.js
@@ -8,21 +8,53 @@ export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-
-
 `
 export const LoginImage = styled.img`
-  height: 85%; // i changed from 70 to 85
-`
+  height: calc( 10rem + 40vw);
+  min-height: 36rem;
+  max-height: 40rem;
+  margin: auto 0 auto calc(10rem - 7vw);
+
+
+  @media screen and (max-width: 1000px) {
+    width: 0px;
+    height: 0px;
+    border: transparent;
+    background-color: transparent;
+  }
+  `
 
 export const ContainerItens = styled.div`
 background: #373737;
 border-radius: 0 10px 10px 0;
-height: 85%; // i changed from 70 to 85
-padding: 25px 75px;
+height: calc( 10rem + 40vw);
+
+padding: calc(2vw) calc(5vw);
 display: flex;
 flex-flow: column nowrap;
 justify-content: center;
+margin: auto calc(10rem - 7vw) auto 0 ;
+width:calc(10rem + 20vw);
+max-height: 40rem;
+max-width: 34rem;
+min-width: 22rem;
+min-height: 36rem;
+
+@media screen and (max-width: 1000px) {
+  border-radius: 10px;
+  padding: 20px 40px;
+ 
+  }
+
+  @media screen and (max-width: 400px) {
+  padding: 20px 20px;
+  margin: auto calc(10rem - 7vw) auto 0;
+  max-height: 40rem;
+  max-width: 34rem;
+  min-width: 19rem;
+  min-height: 36rem;
+  }
+
 
 form{
   display: flex;
@@ -30,13 +62,33 @@ form{
 }
 h1{
   font-family: 'Roboto';
-font-style: normal;
-font-weight: 500;
-font-size: 24px;
-line-height: 28px;
-color: #FFFFFF;
-text-align: center;
-margin-top: 100px;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  color: #FFFFFF;
+  text-align: center;
+  margin-top:  calc(5rem - 2vw);
+  
+}
+
+img{
+  width: calc(5rem + 10vw);
+  margin: 0 auto;
+  min-width: 15rem;
+}
+
+.sign-in-buttton{
+  align-self: center;
+
+  @media screen and (max-width: 1500px) {
+    width: 100%;
+    min-width: 250px;
+  }
+  @media screen and (min-width: 1500px) {
+    
+  }
+
 }
 `
 
@@ -51,13 +103,23 @@ margin-bottom: 5px;
 `
 
 export const Input = styled.input`
-width: 391.42px;
-height: 38.32px;
+width: 20vw;
+height: 1vw;
+min-width: 250px;
+max-width: 391.42px;
+min-height: 38.32px;
 background: #FFFFFF;
 box-shadow: 3px 3px 10px rgba(74, 144, 226, 0.19);
 border-radius: 5px;
 padding-left: 10px;
 border: ${props => (props.error ? '2px  solid #CC1717' : 'none')};
+
+@media screen and (max-width: 1500px) {
+  width: 100%;
+  }
+@media screen and (max-width: 400px) {
+
+  }
 `
 
 export const SignInLink = styled.p`
@@ -66,6 +128,7 @@ font-weight: 300;
 font-size: 14px;
 line-height: 16px;
 color: #FFFFFF;
+align-self: center;
 
 
 a {
